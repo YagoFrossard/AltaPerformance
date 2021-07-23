ef3eb463039e2435cb0958d33033584614ef9b60
DChaotic <daniel.srgomes@hotmail.com>
2021-07-23 14:40:51 -0300

    Testando component com MaterialUI Ver.01

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import SetupTests from "./setupTests";
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <SetupTests/>
       </header>
     </div>
   );
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -2,4 +2,20 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+//import '@testing-library/jest-dom';
+// Componente de teste
+
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const SetupTests = () => {
+    return (
+        <div>
+            <Button variant="contained" color="primary">
+                Butão
+            </Button>
+        </div>
+    );
+};
+
+export default SetupTests;
