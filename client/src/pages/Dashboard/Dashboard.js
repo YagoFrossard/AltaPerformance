b0c0f85157a7c0a5b7b969bf7ce718c60f4c4870
DChaotic <daniel.srgomes@hotmail.com>
2021-08-02 22:17:10 -0300

    Página de Dashboard V1.1

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import {AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 export default function Dashboard() {
-    const [hora, setHora] = useState(Date().toLocaleString());
+    const [hora, setHora] = useState(Date());
+    const [loggedUser, setUser] = useState(null);
+    const classes = useStyles();
 
     const getUser = (e) => {
         e.preventDefault();
@@ -12,28 +29,53 @@ export default function Dashboard() {
                 'Content-Type': 'application/json'
             }, withCredentials: true
         })
-            .then(res => console.log(res.data))
+            .then(res => {console.log(res.data)})
             .catch(err => console.log(err));
     }
 
     useEffect(() => {
+        axios.get('http://localhost:5000/', {
+            headers: {
+                'Content-Type': 'application/json'
+            }, withCredentials: true
+        })
+            .then(res => setUser(res.data))
+            .catch(err => console.log(err));
+    }, [])
+
+    useEffect(() => {
         let interval = null;
         interval = setInterval(() => {
-            setHora(Date().toLocaleString())
+            setHora(Date())
         }, 1000);
         return () => clearInterval(interval);
-        /*setAttr(() => {
-            return <div>Hora : ${attr}</div>
-        })*/
     }, [setHora]);
+
 
     return (
         <div>
+            <AppBar position={"static"} className={classes.root}>
+                <Toolbar>
+                    <IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant={"h6"} className={classes.title}>
+                        Barra de menu
+                    </Typography>
+                    <Typography align={"right"}>
+                        {"Bem-Vindo, " + loggedUser + " "}
+                        {new Date(hora).toLocaleDateString() + " " + new Date(hora).toLocaleTimeString()}
+                        <Button type="submit"
+                                fullWidth
+                                size="small"
+                                variant="contained" >Logout</Button>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
             asda
-            {hora}
             <br />
             teste
-            <button onClick={getUser}></button>
+            <button onClick={getUser}>Console.log usuario</button>
         </div>
     );
 }
